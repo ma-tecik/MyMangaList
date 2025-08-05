ee98a952e563e842696f78220064e54659fc7310
Mehmet Akif Tecik <141300243+ma-tecik@users.noreply.github.com>
2025-08-05 19:29:10 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, jsonify
+import logging
+import os
+import json
+
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_file("config.json", load=json.load)
+
+log_dir = os.path.join(app.instance_path, "logs")
+os.makedirs(log_dir, exist_ok=True)
+
+file_handler = logging.FileHandler(os.path.join(log_dir, "app.log"))
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(logging.Formatter("[%(asctime)s] %(levelname)s in %(module)s.%(funcName)s: %(message)s", datefmt = "%Y-%m-%d %H:%M:%S"))
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+logging.basicConfig(handlers=[file_handler, console_handler], level=logging.INFO)
+
+@app.route("/", methods=["GET"])
+def index():
+    return "", 301, {"Location": "/series/list/plan-to"}
+@app.route('/series/list/<path>', methods=['GET'])
+def series(path):
+    try:
+        if path not in {"plan-to", "reading", "completed", "one-shots", "dropped", "ongoing"}:
+            return {"result": "KO", "error": "Invalid path"}, 404
+        titles = {
+            "plan-to": "Plan to Read",
+            "reading": "Reading",
+            "completed": "Completed",
+            "one-shots": "One-shots",
+            "dropped": "Dropped",
+            "ongoing": "Ongoing",
+        }
+        page_title = titles.get(path)
+        return render_template(
+            "list.html",
+            page_title=page_title,
+            page=path,
+        ), 200
+    except Exception as e:
+        logging.error(str(e))
+        return jsonify({"result": "KO", "error": "Internal server error"}), 500
---
+++ schema.sql
@@ -0,0 +1,149 @@
+create table authors
+(
+    id          INTEGER
+        primary key autoincrement,
+    id_mu       TEXT
+        unique,
+    id_dex      TEXT
+        unique,
+    id_mal      INTEGER
+        unique,
+    nationality TEXT,
+    gender      TEXT,
+    check (gender in ('f', 'm', 'n'))
+);
+
+create table genres
+(
+    id    INTEGER
+        primary key autoincrement,
+    genre TEXT not null
+        unique
+);
+
+create table nhentai_ids
+(
+    nhentai_id INTEGER not null
+        primary key
+);
+
+create table schale_ids
+(
+    schale_id  INTEGER not null
+        primary key,
+    schale_key TEXT    not null
+);
+
+create table series
+(
+    id           INTEGER
+        primary key autoincrement,
+    id_mu        TEXT
+        unique,
+    id_dex       TEXT
+        unique,
+    id_bato      INTEGER
+        unique,
+    id_mal       INTEGER
+        unique,
+    id_line      INTEGER
+        unique,
+    title        TEXT not null,
+    type         TEXT not null,
+    description  TEXT,
+    vol_ch       TEXT,
+    year         INTEGER,
+    timestamp_mu INTEGER,
+    is_md        BOOLEAN default 0,
+    status       TEXT not null,
+    user_rating  REAL
+        check (status in ('Plan to Read', 'Reading', 'Completed', 'One-shot', 'Dropped', 'Ongoing'))
+);
+
+create table series_titles
+(
+    id        INTEGER
+        primary key autoincrement,
+    series_id INTEGER not null
+        references series
+            on delete cascade,
+    alt_title TEXT    not null,
+    unique (series_id, alt_title)
+);
+
+create index idx_alt_titles_series_id
+    on series_titles (series_id);
+
+create table series_authors
+(
+    series_id   INTEGER               not null
+        references series
+            on delete cascade,
+    author_id   INTEGER               not null
+        references authors
+            on delete cascade,
+    author_type TEXT default 'Author' not null,
+    primary key (series_id, author_id, author_type),
+    check (author_type IN ('Author', 'Artist', 'Both'))
+);
+
+create index series_authors_series_id
+    on series_authors (series_id);
+
+create table series_genres
+(
+    series_id INTEGER not null
+        references series
+            on delete cascade,
+    genre_id  INTEGER not null
+        references genres
+            on delete cascade,
+    primary key (series_id, genre_id)
+);
+
+create index series_genres_series_id
+    on series_genres (series_id);
+
+create table series_images
+(
+    extension TEXT    not null,
+    series_id integer not null
+        primary key
+        references series
+            on delete cascade
+);
+
+create table series_nhentai_ids
+(
+    series_id  INTEGER not null
+        references series
+            on delete cascade,
+    nhentai_id INTEGER not null
+        references nhentai_ids
+            on delete cascade,
+    primary key (series_id, nhentai_id)
+);
+
+create table series_ratings_mu
+(
+    id_mu  INTEGER not null
+        primary key
+        references series (id_mu)
+            on delete cascade,
+    rating REAL    not null,
+    votes  INTEGER not null
+);
+
+create index idx_series_ratings_rating
+    on series_ratings_mu (rating);
+
+create table series_schale_ids
+(
+    series_id INTEGER not null
+        references series
+            on delete cascade,
+    schale_id INTEGER not null
+        references schale_ids
+            on delete cascade,
+    primary key (series_id, schale_id)
+);
